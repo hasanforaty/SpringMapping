b21978fca94b69a8134bd37bc0995db4b7edd8f1
hasan foraty <hasanforaty@gmail.com>
2023-09-14 15:19:03 +0330

    add config for :  - runner  - mysql config

--- src/main/java/com/hasan/foraty/springmapping/SpringMappingApplication.java
+++ src/main/java/com/hasan/foraty/springmapping/SpringMappingApplication.java
@@ -1,7 +1,9 @@
 package com.hasan.foraty.springmapping;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringMappingApplication {
@@ -9,5 +11,15 @@ public class SpringMappingApplication {
   public static void main(String[] args) {
     SpringApplication.run(SpringMappingApplication.class, args);
   }
+
+
+  @Bean
+  public CommandLineRunner commandLineRunner (){
+    return runner ->{
+      System.out.println("hello word!");
+    };
+  }
+
+
 
 }
